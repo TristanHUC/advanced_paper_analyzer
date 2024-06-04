aba484a252c7734c72cc70c2cd3c0a1003a954f6
TristanHUC <tristan.hucher@gmail.com>
2024-06-04 15:16:37 +0200

    adapting grobid to docker compose network

--- Scripts/script_grobit_pdf.py
+++ Scripts/script_grobit_pdf.py
@@ -3,7 +3,7 @@ from grobid_client_python.grobid_client.grobid_client import GrobidClient
 #make sure to have grobid running before executing this script
 def process_pdf(URL_Grobid):
     try :
-        client = GrobidClient(grobid_server = URL_Grobid, config_path="grobid_client_python/config.json")
+        client = GrobidClient(grobid_server = URL_Grobid, batch_size = 100,coordinates=[ "persName", "figure", "ref", "biblStruct", "formula", "s", "note", "title" ])
         client.process("processFulltextDocument",  "Corpus_pdf/", output="Grobid_processed_pdf/", tei_coordinates=True, force=True)
         return ['data processed sucessfully']
     except  Exception as e:
